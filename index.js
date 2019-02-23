0abbb1f998e389397c31b9013118609b324e291f
CalvinRodo <calvinv@gmail.com>
2019-02-22 22:38:31 -0500

    Added eslint

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
   await page.setBypassCSP(true);
 
-  await page.goto("https://dequeuniversity.com/demo/mars/");
+  await page.goto("https://www.canada.ca/en/employment-social-development/programs/ei/ei-list/ei-roe/access-roe.html");
 
   const results = await new AxePuppeteer(page).analyze();
   console.log(results);
