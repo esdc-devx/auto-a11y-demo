e8387ec0a041d8f08df5a81c43741a4198d21044
s-laugh <shaunlaughland@gmail.com>
2019-03-01 13:50:51 -0500

    adding and fixing assistant tests works on #9 tests still not running consistantly

--- src/index.js
+++ src/index.js
@@ -27,6 +27,7 @@ async function runtest(page, name) {
   const password = process.env.ROEWEB_PWORD;
 
   const browser = await puppeteer.launch({ headless: true });
+  //const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
   console.log("Getting New Page");
   const page = await browser.newPage();
   console.log("Bypassing CSP");
@@ -128,57 +129,150 @@ async function runtest(page, name) {
   );
   await runtest(page, "AssistantIntroductionWelcome");
   //new roe
+  page.click(
+    "body > div > div.row > main > fieldset > form > div:nth-child(2) > label > input[type=radio]"
+  );
   await Promise.all([
-    page.click(
-      "body > div > div.row > main > fieldset > form > div:nth-child(2) > label > input[type=radio]"
-    )
+    page.click("button[type=submit]"),
+    page.waitForSelector("body > div > div.row > main > a.btn.btn-primary")
   ]);
-  await page.click("body > div > div.row > main > fieldset > form > button");
-  await page.waitForNavigation();
   await runtest(page, "AssistantIntroductionNewROE");
   //confirm user
-  await page.click("body > div > div.row > main > a.btn.btn-primary");
-  await page.waitForNavigation();
+  await Promise.all([
+    page.click("body > div > div.row > main > a.btn.btn-primary"),
+    page.waitForNavigation()
+  ]);
   await runtest(page, "AssistantConfirmUser");
   //employer
+  await page.click(
+    "body > div > div.row > main > form > fieldset > div:nth-child(3) > label > input[type=radio]"
+  );
   await Promise.all([
-    page.click(
-      "body > div > div.row > main > form > fieldset > div:nth-child(3) > label > input[type=radio]"
-    )
+    page.waitForNavigation(),
+    page.click("body > div > div.row > main > form > button")
   ]);
-  await page.click("body > div > div.row > main > form > button");
-  await page.waitForNavigation();
   await runtest(page, "AssistantEmployerInfo");
   //employee
-  await Promise.all([page.select("#OrgBusinessId", "117816201")]);
-  await Promise.all([page.type("#Address_Line1", "123 AutoTest St")]);
-  await Promise.all([page.type("#Address_City", "AutoTest")]);
   await Promise.all([
-    page.select("#Address_ProvinceState", "AB"),
-    page.select("#Address_Country", "CA"),
-    page.type("#Address_PostalCode", "K1T3S7")
+    page.select("#OrgBusinessId", "117816201"),
+    page.waitForFunction(
+      "document.querySelector('#Contact').offsetParent !== null"
+    ) //Waiting for the contact div to be visible.
   ]);
-  await Promise.all([page.type("#Contact_FirstName", "Auto")]);
-  await Promise.all([page.type("#Contact_LastName", "Test")]);
-  await Promise.all([page.type("#Contact_AreaCode", "999")]);
-  await Promise.all([page.type("#Contact_PhoneNumber", "9999999")]);
-  await page.click(
-    "body > div > div.row > main > div.roe-wb-frmvld > form > button"
-  );
-  await page.waitForNavigation();
+  // await page.type("#Address_Line1", "123 AutoTest St");
+  // await page.type("#Address_City", "AutoTest");
+  // await page.select("#Address_ProvinceState", "AB");
+  // await page.select("#Address_Country", "CA");
+  // await page.type("#Address_PostalCode", "K1T3S7");
+  await page.type("#Contact_FirstName", "Auto");
+  await page.type("#Contact_LastName", "Test");
+  await page.type("#Contact_AreaCode", "999");
+  await page.type("#Contact_PhoneNumber", "9999999");
+  await Promise.all([
+    page.click(
+      "body > div > div.row > main > div.roe-wb-frmvld > form > button"
+    ),
+    page.waitForSelector("#Sin")
+  ]);
   await runtest(page, "AssistantEmployeeInfo");
   //employment
-  await Promise.all([page.type("#Sin", "123456782")]);
-  await Promise.all([page.type("#FirstName", "Auto")]);
-  await Promise.all([page.type("#LastName", "Test")]);
-  await Promise.all([page.type("#AddressLine1", "123 AutoTest St")]);
-  await Promise.all([page.type("#AddressLine2", "AutoTest")]);
-  await Promise.all([page.type("#PostalCode", "K1T3S7")]);
-  await page.click(
-    "body > div > div.row > main > div.roe-wb-frmvld > form > button"
-  );
-  await page.waitForNavigation();
+  await page.type("#Sin", "990000002");
+  console.log("sin done");
+  await page.type("#FirstName", "Auto");
+  await page.type("#LastName", "Test");
+  await page.type("#AddressLine1", "123 AutoTest St");
+  await page.type("#AddressLine2", "AutoTest");
+  await page.type("#PostalCode", "K1T3S7");
+  await Promise.all([
+    page.click(
+      "body > div > div.row > main > div.roe-wb-frmvld > form > button"
+    ),
+    page.waitForNavigation()
+  ]);
   await runtest(page, "AssistantEmploymentInfo");
+  //reason for issue
+  await Promise.all([
+    page.select("#PayPeriod", "M"),
+    page.waitForFunction(
+      "document.querySelector('#PayPeriodDates').offsetParent !== null"
+    ) //Waiting for the pay period dates div to be visible.
+  ]);
+  await page.type("#FirstDayWorked", "01012018");
+  await page.type("#LastDayPaid", "28022018");
+  await page.type("#FinalPayPeriodEndDate", "28022018");
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest(page, "AssistantReasonForIssuing");
+  //employment details
+  await page.select("#ReasonForIssuing", "A");
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest("AssistantEmploymentDetails");
+  //special payments
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest("AssistantSpecialPayments");
+  //vacation payments
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest("AssistantVacationPayments");
+  //stat holliday payments
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest("AssistantStatHolidayPayments");
+  //earnings
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest("AssistantEarningsPayPeriod");
+  //insurable hours
+  await page.type("#stdAutoFillAmount", "1000");
+  await Promise.all([
+    page.click("#stdAutoFillButton"),
+    page.waitForFunction("autoFillClick") //wait for the function from the click to run
+  ]);
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest(page, "AssistantInsurableHours");
+  //total insurable amounts
+  await page.type("#TotalInsurableHours", "100");
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest(page, "AssistantTotalInsurableAmounts");
+  //ready to submit
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest(page, "AssistantReadySubmit");
+  //r&s completed roe
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest(page, "AssistantRasCompletedRoe");
+  //submission status
+  await page.click("#Declaration");
+  await Promise.all([
+    page.click("button[type=submit]"),
+    page.waitForNavigation()
+  ]);
+  await runtest(page, "AssistantSubmissionStatus");
 
   await page.goto(
     "https://srv136.services.gc.ca/ROE-RE/ROEWeb-REWeb/pro/Search/Draft?org_id=-" +
